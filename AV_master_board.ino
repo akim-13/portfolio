587448809245a1fc50f1bf4ab5572b476c839298
sku4no <148970325+sku4no@users.noreply.github.com>
2023-11-30 22:03:05 +0000

    AV_master_board.ino

--- AV_master_board.ino
+++ AV_master_board.ino
@@ -16,26 +16,35 @@ void setup()
 void flashLED (int pinLED)
 {
     digitalWrite(pinLED, HIGH);
-    delay(500);
+    delay(1000);
     digitalWrite(pinLED, LOW);
 }
 
-void loop() 
-{
+void loop() {
     // Request 1 byte from the slave board with address 4.
     Wire.requestFrom(4, 1); 
   
-    if (Wire.available()) 
-    {
+    if (Wire.available()) {
         // Read the count from the slave board.
         int peopleInRoom = Wire.read(); 
-        if (peopleInRoom > lastPeopleInRoom) 
+        Serial.print("People in Room: ");
+        Serial.println(peopleInRoom);
+        
+        if (peopleInRoom > lastPeopleInRoom) {
+            Serial.println("Someone entered");
             flashLED (enterLED);
-        else 
+        } else if (peopleInRoom < lastPeopleInRoom) {
+            Serial.println("Someone exited");
             flashLED (exitLED);
+        }
 
         lastPeopleInRoom = peopleInRoom; 
+    } else {
+        Serial.println("No data available from slave");
     }
+
+    delay(1000); // Wait for a second before making the next request
+}
 
     delay(1000); 
 }
