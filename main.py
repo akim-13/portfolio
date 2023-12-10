8d3493bb8a74f14f5d6a5ea2813af0302ad14966
Akim <akimkom@protonmail.com>
2023-12-10 12:49:33 +0000

    Did menu and option picking

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import sqlite3
 import logging
 import inspect
 
-############ DEBUGGING ############ 
+
+#################### DEBUGGING ####################
 lvl = logging.DEBUG 
 fmt = '%(lineno)s: [%(levelname)s] %(msg)s'
 logging.basicConfig(level = lvl, format = fmt)
@@ -18,11 +19,14 @@ def w(log):
 def e(log):
     frame = inspect.currentframe().f_back
     logging.getLogger().error(log, stacklevel=2)
-################################### 
+###################################################
 
+
+# Shortcuts for using SQL.
 db = sqlite3.connect('airline.db') 
 cursor = db.cursor()
 sql = lambda query: cursor.execute(query)
+
 
 def create_tables():
     # Create table Aircrafts.
@@ -68,11 +72,78 @@ def create_tables():
 
     db.commit()
 
-    i('Airline database and tables created successfully.')
+
+def display_menu(options):
+    print('Available options:')
+    for key, value in sorted(options.items()):
+        print(f'{key}. - {value[0]}')
+
+
+def quit_app():
+    print("Thank you for using AirlineDB!")
+    return
+
+
+def insert_data():
+    pass
+
+
+def search_data():
+    pass
+
+
+def update_data():
+    pass
+
+
+def delete_data():
+    pass
+
+
+def choose_summary_statistics():
+    pass
+
+
+def extra1():
+    pass
+
+
+def extra2():
+    pass
+    
+
+options = {
+    0: ("Quit", quit_app),
+    1: ("Insert", insert_data),
+    2: ("Search", search_data),
+    3: ("Update", update_data),
+    4: ("Delete", delete_data),
+    5: ("Summary Statistics", choose_summary_statistics),
+    6: ("Extra 1", extra1),
+    7: ("Extra 2", extra2)
+}
+
 
 def main():
-
     create_tables()
+
+    inp = None 
+    while inp != 0:
+        display_menu(options)
+
+        inp = input('>> ')
+
+        # Verify that the input is valid.
+        try:
+            inp = int(inp)
+            num_of_options = len(options)
+            if (inp < 0) or (inp >= num_of_options): raise
+        except:
+            print("ERROR: Please enter a valid integer.\n")
+            continue
+
+        selected_option = options.get(inp)
+        execute_selected_option = selected_option[1]()
 
     db.close()
 
