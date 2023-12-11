bcb38aab15c216e9888e5e5ec702eb0df6c5eb90
Akim <akimkom@protonmail.com>
2023-12-11 20:40:44 +0000

    Completed data insertion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 from datetime import datetime
 from my_logger import d, i, w, e
 
@@ -15,11 +16,15 @@ def verify_input(inp, lower_bound, upper_bound):
     try:
         inp = int(inp)
         if (inp < lower_bound) or (inp > upper_bound): 
-            raise
+            raise IndexError
         return inp
-    except:
-        print("ERROR: Please enter a valid integer.")
-        return None
+    except ValueError:
+        print(f'ERROR: "{inp}" is not an integer.', end=' ')
+    except IndexError:
+        print(f'ERROR: {inp} is is out of range.', end=' ')
+
+    print('No data has been saved.')
+    return None
 
 
 def prompt_for_valid_input(lower_bound, upper_bound):
@@ -34,9 +39,9 @@ def prompt_for_valid_input(lower_bound, upper_bound):
 
 
 def create_tables():
-    # Create table Aircrafts.
+    # Create table Aircraft.
     sql('''
-    CREATE TABLE IF NOT EXISTS Aircrafts (
+    CREATE TABLE IF NOT EXISTS Aircraft (
         aircraftID INT PRIMARY KEY,
         maxCapacity INT,
         accessible BOOLEAN,
@@ -53,7 +58,7 @@ def create_tables():
         destination TEXT,
         departureTime INT,  -- UNIX time, DATETIME is not supported.
         landingTime INT,
-        FOREIGN KEY (aircraftID) REFERENCES Aircrafts(aircraftID))
+        FOREIGN KEY (aircraftID) REFERENCES Aircraft (aircraftID) ON UPDATE CASCADE)
     ''')
 
     # Create table Pilots.
@@ -62,7 +67,7 @@ def create_tables():
         pilotID INT PRIMARY KEY,
         firstName TEXT,
         surname TEXT,
-        dob TEXT)
+        dob INT)
     ''')
 
     # Create table AssignedPilots.
@@ -71,8 +76,8 @@ def create_tables():
         pilotID INT,
         flightID INT,
         PRIMARY KEY (pilotID, flightID),
-        FOREIGN KEY (pilotID) REFERENCES Pilots(pilotID),
-        FOREIGN KEY (flightID) REFERENCES Flights(flightID))
+        FOREIGN KEY (pilotID) REFERENCES Pilots(pilotID) ON UPDATE CASCADE,
+        FOREIGN KEY (flightID) REFERENCES Flights(flightID) ON UPDATE CASCADE)
     ''')
 
     db.commit()
@@ -81,7 +86,7 @@ def create_tables():
 def display_menu(options):
     print('\nAvailable options:')
     for key, value in sorted(options.items()):
-        print(f'{key}. - {value[0]}')
+        print(f'{key}. {value[0]}')
 
 
 def quit_app():
@@ -95,10 +100,6 @@ def select_table_name(tables):
 
     for i, table in enumerate(tables, 1):
         table_name = table[0]
-
-        # Skip the bridging table.
-        if table_name == 'AssignedPilots': 
-            continue
 
         print(f'{i}. {table_name}')
         available_tables[i] = table_name
@@ -125,6 +126,20 @@ def view_table():
     print(cursor.fetchall())
 
 
+def convert_to_unix_time(inp):
+    try:
+        # Ensure that the format is correct.
+        valid_datetime = datetime.strptime(inp, '%d/%m/%Y %H:%M')
+        # Convert to UNIX time (the number of seconds since January 1, 1970).
+        unix_time = int(valid_datetime.timestamp())
+        if unix_time < 0:
+            raise ValueError
+
+        return unix_time
+    except ValueError:
+        return None
+
+
 def insert_data():
     sql('SELECT name FROM sqlite_master WHERE type="table"')    
     tables = cursor.fetchall()
@@ -134,47 +149,144 @@ def insert_data():
     sql(f'PRAGMA table_info({table_name})')
     columns = cursor.fetchall()
 
+    # Query for foreign keys in the table.
+    sql(f'PRAGMA foreign_key_list({table_name})')
+    foreign_keys_info = cursor.fetchall()
+    # Create a dictionary to map foreign key columns to their referenced tables and columns
+    # (column 3 contains the foreign key column name, column 2 the referenced table, column 4 the referenced column)
+    foreign_key_references = { fk_info[3]: (fk_info[2], fk_info[4]) for fk_info in foreign_keys_info }
+
     # Holds the input values to be inserted in the SQL query.
     values_to_be_inserted = []
 
     for column in columns:
         # Column format: (cid, name, type, notnull, dflt_value, pk).
-        column_name = column[1]
-        column_type = column[2].lower()
-        column_notnull = column[3]
-        column_is_pk = column[5]
+        _, column_name, column_type, column_notnull, _, column_is_pk = column
+        # Make column type lowercase.
+        column_type = column_type.lower()
 
-        print(f'Please enter "{column_name}"', end='')
+        input_is_valid = False
 
-        # Account for both "INT" and "INTEGER", same with "BOOL".
-        if (column_type.startswith('int')):
-            print(' (or -1 for NULL).')
-            inp = prompt_for_valid_input(-1, INFINITY)
-            if (inp == -1):
-                if (column_notnull == 0) and (column_is_pk == 0):
-                    inp = None
+        # If the column is a primary key.
+        # All primary keys (unless also a FK) are auto generated random integers.
+        if (column_is_pk) and (column_name not in foreign_key_references):
+            while (True):
+                # TODO: Change back, for testing purposes only.
+                generated_primary_key = random.randint(1, 100)
+                # generated_primary_key = random.randint(1000000000, 9999999999)
+                sql(f'SELECT {column_name} FROM {table_name}')
+                # Flatten the list of fetched tuples.
+                primary_keys = { key[0] for key in cursor.fetchall() }
+                if generated_primary_key in primary_keys:
+                    print('Congratulations! You have successfully generated a non-unique 10 digit primary key.')
+                    print('Please consider buying a lottery ticket.')
                 else:
-                    print(f'ERROR: "{column_name}" cannot be NULL.')
-                    return
+                    # To be added to the `values_to_be_inserted` list later.
+                    inp = generated_primary_key
+                    input_is_valid = True
+                    break
 
-        elif (column_type.startswith('bool')):
-            print(' (1 - TRUE, 0 - FALSE).')
-            inp = prompt_for_valid_input(-1, 1)
-            if (inp == -1):
-                if (column_notnull == 0) and (column_is_pk == 0):
-                    inp = None
+        if not input_is_valid:
+            print(f'Please enter "{column_name}"', end='')
+
+        is_first_attempt = True
+        while (not input_is_valid):
+            # Handle dates and time.
+            if (column_name == 'dob') or (column_name.endswith("Time")):
+                if is_first_attempt:
+                    # DOB will also include `hh:mm`, however the user can just enter `00:00` or anything else.
+                    print(' (exactly "DD/MM/YYYY hh:mm" or -1 for NULL).')
+                is_first_attempt = False
+
+                inp = input(" >>> ")
+
+                # Check if -1 has been entered.
+                try:
+                    inp = int(inp)
+                    if (inp == -1):
+                        # Check if the value can be assigned NULL.
+                        if not column_notnull:
+                            inp = None
+                            input_is_valid = True
+                        else:
+                            print(f'ERROR: "{column_name}" cannot be NULL. No data has been saved.')
+                            input_is_valid = False
+                    else:
+                        print("ERROR: Invalid date format. No data has been saved.")
+                        input_is_valid = False
+                # Otherwise handle the entered date.
+                except ValueError:
+                    inp = convert_to_unix_time(inp)
+                    if inp == None:
+                        print("ERROR: Invalid date format. No data has been saved.")
+                        input_is_valid = False
+                    else:
+                        input_is_valid = True
+
+            # Handle `INTEGER` data type.
+            # Account for both "INT" and "INTEGER", same with "BOOL".
+            elif column_type.startswith('int'):
+                # Hints are only displayed on the first attempt.
+                if is_first_attempt:
+                    print(' (or -1 for NULL).')
+                is_first_attempt = False
+
+                inp = prompt_for_valid_input(-1, INFINITY)
+
+                if (inp == -1):
+                    # Check if the value can be assigned NULL.
+                    if not column_notnull:
+                        inp = None
+                        input_is_valid = True
+                    else:
+                        print(f'ERROR: "{column_name}" cannot be NULL. No data has been saved.')
+                        input_is_valid = False
                 else:
-                    print(f'ERROR: "{column_name}" cannot be NULL.')
-                    return
+                    input_is_valid = True
 
-        elif (column_name == 'dob') or (column_name.endswith("Time")):
-            print(' (YYYY-MM-DD etc).')
-            # TODO: Implement date picking.
-            inp = 0
+            # Handle `BOOLEAN` data type.
+            elif column_type.startswith('bool'):
+                print(' (1 - TRUE, 0 - FALSE).')
+                inp = prompt_for_valid_input(0, 1)
+                input_is_valid = True
+                
+            # Handle `TEXT` data type.
+            else:
+                if is_first_attempt:
+                    print(" (or -1 for NULL).")
+                is_first_attempt = False
 
-        else:
-            print(".")
-            inp = input(' >>> ')
+                inp = input(' >>> ')
+
+                # Check if -1 has been entered.
+                try:
+                    inp = int(inp)
+                    if (inp == -1):
+                        # Check if the value can be assigned NULL.
+                        if not column_notnull:
+                            inp = None
+                            input_is_valid = True
+                        else:
+                            print(f'ERROR: "{column_name}" cannot be NULL. No data has been saved.')
+                            input_is_valid = False
+                    else:
+                        inp = str(inp)
+                        input_is_valid = True
+                # Any other input is valid, since it is a string.
+                except ValueError:
+                    input_is_valid = True
+
+            # Check if the column is a foreign key and validate the input.
+            if column_name in foreign_key_references:
+                referenced_table, referenced_column = foreign_key_references[column_name]
+                sql(f'SELECT EXISTS(SELECT 1 FROM {referenced_table} WHERE {referenced_column} = ?)', (inp,))
+                exists = cursor.fetchone()[0]
+                if not exists:
+                    print(f'ERROR: "{inp}" does not exist in referenced table "{referenced_table}".')
+                    input_is_valid = False
+                else:
+                    input_is_valid = True
+
 
         values_to_be_inserted.append(inp)
 
@@ -182,10 +294,11 @@ def insert_data():
     placeholders = ', '.join(['?'] * len(values_to_be_inserted))
     # Construct the column names string.
     column_names = ', '.join(column[1] for column in columns)
-    d(placeholders)
-    d(column_names)
 
-    sql(f'INSERT INTO {table_name} ({column_names}) VALUES ({placeholders})', values_to_be_inserted)
+    try:
+        sql(f'INSERT INTO {table_name} ({column_names}) VALUES ({placeholders})', values_to_be_inserted)
+    except Exception as e:
+        print(f'ERROR: {e}. Please try again.')
 
     db.commit()
 
