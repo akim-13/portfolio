393e45030be8b17f77002996011c180bdaed3679
Akim <akimkom@protonmail.com>
2024-01-03 15:57:57 +0100

    Finished constrained propagation

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import numpy as np
+import time
+
+class ContradictionException(Exception):
+    pass
+
 
 # (1, 1) (1, 2) (1, 3)   (1, 4) (1, 5) (1, 6)   (1, 7) (1, 8) (1, 9)
 # (2, 1) (2, 2) (2, 3)   (2, 4) (2, 5) (2, 6)   (2, 7) (2, 8) (2, 9)
@@ -46,13 +51,45 @@ for cell in cells:
 
 def remove_digit_from_peers(digit, cell, grid):
     for peer in peers[cell]:
-        # TODO: Figure out if it's a valid case and what to do with it.
+        # TODO: Figure out if this ever happens what to do with it.
         # If there are no available digits for given a cell.
         if not grid[peer]:
-            return None
+            raise ContradictionException
         grid[peer] = grid[peer].replace(digit, '')
     return grid
 
+
+def assign_and_propagate(digit, cell, grid):
+    # When the cell is empty (`digit == 0`) it has to keep the default `available_digits` value.
+    if digit != '0':
+        grid[cell] = digit
+    
+    cell_has_one_possible_value = len(grid[cell]) == 1
+    if cell_has_one_possible_value:
+        grid = remove_digit_from_peers(digit, cell, grid)
+
+    digits_counter = {digit: {'count': 0, 'cell': None} for digit in '123456789'}
+    # Insert a digit if a unit has one possible place for a value.
+    for unit in units[cell]:
+        for cell_iter in unit:
+            for digit_iter in grid[cell_iter]:
+                digits_counter[digit_iter]['count'] += 1
+                digits_counter[digit_iter]['cell'] = cell_iter
+
+        for digit_key, value in digits_counter.items():
+            unit_missing_possible_digit = value['count'] == 0
+            if unit_missing_possible_digit: 
+                raise ContradictionException
+
+            cell_has_multiple_possible_values = len(grid[value['cell']]) > 1
+            unit_has_one_possible_place_for_value = (value['count'] == 1) and (cell_has_multiple_possible_values)
+            if unit_has_one_possible_place_for_value:
+                assign_and_propagate(digit_key, value['cell'], grid)
+
+            # Reset the values for the iteration over the next unit.
+            digits_counter[digit_key]['count'] = 0
+            digits_counter[digit_key]['cell'] = None
+                
 
 def generate_solution(numpy_arrays):
     flattened_numpy_array = [ str(item_cell) for array in numpy_arrays for item_cell in array ]
@@ -60,31 +97,87 @@ def generate_solution(numpy_arrays):
     # Initialize an empty greed (i.e. every cell can take any digit).
     grid = { cell: available_digits for cell in cells }
 
-    print(grid)
-    first_assignment = True
     for cell, digit in zip(cells, flattened_numpy_array):
-        # When the cell is empty (`digit == 0`) it has to keep the default `available_digits` value.
-        if digit != '0':
-            grid[cell] = digit
-        
-        cell_has_one_possible_value = len(grid[cell]) == 1
-        if cell_has_one_possible_value:
-            remove_digit_from_peers(digit, cell, grid)
-
-        # TODO: Insert a digit if a unit has one possible place for a value.
-
-    print()
-    print(grid)
-
+        assign_and_propagate(digit, cell, grid)
 
     return grid
 
+
+def convert_grid_to_np_array(grid):
+    np_grid = np.empty((9, 9), dtype=object)
+
+    for (row, col), available_digits in grid.items():
+        try:
+            available_digits = int(available_digits)
+            # print(available_digits)
+        except:
+            available_digits = '●'
+        np_grid[row-1, col-1] = available_digits
+    return np_grid
+
+
+def display_sudoku(grid):
+    horizontal_line = ' ' + '+'.join(['-'*7]*3)
+    for i, row in enumerate(grid):
+        if i % 3 == 0 and i != 0:
+            print(horizontal_line)
+        try:
+            row_str = '| ' + ' | '.join(' '.join(str(int(cell)) for cell in row[j:j+3]) for j in range(0, 9, 3)) + ' |'
+        except:
+            row_str = '| ' + ' | '.join(' '.join(str(cell) for cell in row[j:j+3]) for j in range(0, 9, 3)) + ' |'
+        print(row_str)
+    print()
+
+
 def main():
-    easy_sudokus = np.load("data/very_easy_puzzle.npy")
-    easy_sudokus_solutions = np.load("data/very_easy_solution.npy")
-    print(easy_sudokus[0])
-    generate_solution(easy_sudokus[0])
-    print(easy_sudokus_solutions[0])
+    difficulty = 'medium'
+    sudokus = np.load(f'data/{difficulty}_puzzle.npy')
+    sudokus_solutions = np.load(f'data/{difficulty}_solution.npy')
+
+    # for x, i in enumerate(sudokus_solutions):
+    #     for y, j in enumerate(i):
+    #         for z, k in enumerate(j):
+    #             sudokus_solutions[x][y][z] = int(k)
+
+    # Convert solutions to int if necessary
+    sudokus_solutions = sudokus_solutions.astype(int)
+
+    solving_times = []
+
+    for i in range(0, 15):
+        print('Sudoku:')
+        display_sudoku(sudokus[i])
+
+        start_time = time.time()
+
+        try:
+            solution = generate_solution(sudokus[i])
+            solution = convert_grid_to_np_array(solution)
+        except ContradictionException:
+            solution = np.full((9, 9), -1)
+
+        end_time = time.time()
+        solving_time = end_time - start_time
+        solving_times.append(solving_time)
+
+        print(f'Solving time: {solving_time} seconds')
+        print('Generated solution:')
+        display_sudoku(solution)
+
+        print('Actual solution:')
+        display_sudoku(sudokus_solutions[i])
+
+        if not np.array_equal(solution, sudokus_solutions[i]):
+            raise ContradictionException
+
+        print('---------------------------------------------------------------------------------')
+
+    # Calculating and printing average and maximum times
+    average_time = sum(solving_times) / len(solving_times)
+    max_time = max(solving_times)
+    print(f'Average solving time: {average_time} seconds')
+    print(f'Maximum solving time: {max_time} seconds')
+
 
 if __name__ == "__main__":
     main()
