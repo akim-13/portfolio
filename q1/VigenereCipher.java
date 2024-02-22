7cdd53f9041f0b4e4a90f450fb346e5a2c19efd0
Akim <akimkom@protonmail.com>
2024-02-22 14:03:33 +0000

    [Q1] Started encrypt function

--- q1/VigenereCipher.java
+++ q1/VigenereCipher.java
@@ -31,9 +31,76 @@ interface Cipher {
 }
 
 public class VigenereCipher implements Cipher {
+    private static final String[] vigenereSquare = {
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        "BCDEFGHIJKLMNOPQRSTUVWXYZA",
+        "CDEFGHIJKLMNOPQRSTUVWXYZAB",
+        "DEFGHIJKLMNOPQRSTUVWXYZABC",
+        "EFGHIJKLMNOPQRSTUVWXYZABCD",
+        "FGHIJKLMNOPQRSTUVWXYZABCDE",
+        "GHIJKLMNOPQRSTUVWXYZABCDEF",
+        "HIJKLMNOPQRSTUVWXYZABCDEFG",
+        "IJKLMNOPQRSTUVWXYZABCDEFGH",
+        "JKLMNOPQRSTUVWXYZABCDEFGHI",
+        "KLMNOPQRSTUVWXYZABCDEFGHIJ",
+        "LMNOPQRSTUVWXYZABCDEFGHIJK",
+        "MNOPQRSTUVWXYZABCDEFGHIJKL",
+        "NOPQRSTUVWXYZABCDEFGHIJKLM",
+        "OPQRSTUVWXYZABCDEFGHIJKLMN",
+        "PQRSTUVWXYZABCDEFGHIJKLMNO",
+        "QRSTUVWXYZABCDEFGHIJKLMNOP",
+        "RSTUVWXYZABCDEFGHIJKLMNOPQ",
+        "STUVWXYZABCDEFGHIJKLMNOPQR",
+        "TUVWXYZABCDEFGHIJKLMNOPQRS",
+        "UVWXYZABCDEFGHIJKLMNOPQRST",
+        "VWXYZABCDEFGHIJKLMNOPQRSTU",
+        "WXYZABCDEFGHIJKLMNOPQRSTUV",
+        "XYZABCDEFGHIJKLMNOPQRSTUVW",
+        "YZABCDEFGHIJKLMNOPQRSTUVWX",
+        "ZABCDEFGHIJKLMNOPQRSTUVWXY",
+    };
+
+    private static final String alphabetLength = vigenereSquare[0].length();
+
+    private static int getLetterIndex(char letter) {
+        int letterIndex = Character.toLowerCase(letter) - 'a';
+        // TODO: Check if letter index is in bounds.
+        // if (letterIndex < 0 || letterIndex > ...)
+        return letterIndex
+    }
+
     @Override
     public String encrypt(String messageFilename, String keyFilename) {
-        return "hi"; // Placeholder implementation
+        String message = VigenereCipher.getFileContents(messageFilename);
+        String key = VigenereCipher.getFileContents(keyFilename);
+
+        StringBuilder modifiedKey = new StringBuilder(key);
+        while (modifiedKey.length() < message.length()) {
+            modifiedKey.append(key);
+        }
+
+        if (modifiedKey.length() > message.length()) {
+            modifiedKey.setLength(message.length());
+        }
+        key = modifiedKey.toString();
+
+        StringBuilder encryptedMessageBuilder = new StringBuilder();
+
+        for (int i = 0; i < message.length(); i++) {
+            char currentMessageLetter = message.charAt(i);
+            char currentKeyLetter = key.charAt(i);
+
+            int vigenereRowIndex = VigenereCipher.getLetterIndex(currentKeyLetter);
+            int vigenereColumnIndex = VigenereCipher.getLetterIndex(currentMessageLetter);
+
+            if (vigenereRowIndex || vigenereColumnIndex > VigenereCipher.alphabetLength - 1){ }
+
+            char encryptedLetter = VigenereCipher.vigenereSquare[vigenereRowIndex].charAt(vigenereColumnIndex);
+            encryptedMessageBuilder.append(encryptedLetter);
+        }
+
+        String encryptedMessage = encryptedMessageBuilder.toString();
+        return "hi";
     }
 
     @Override
@@ -41,34 +108,46 @@ public class VigenereCipher implements Cipher {
         return "hello"; // Placeholder implementation
     }
 
-    public static void main(String[] args) {
-        VigenereCipher cipher = new VigenereCipher();
-        String keyFilename = "key_check.txt";
-        String messageFilename = "encrypt_check.txt";
-        String encryptedMessageCheckFilename = "decrypt_check.txt";
-
-        StringBuilder originalMessageBuilder = new StringBuilder();
+    private static String getFileContents(String filename) {
+        StringBuilder stringBuilder = new StringBuilder();
         
         try {
-            File file = new File(messageFilename);
+            File file = new File(filename);
             Scanner scanner = new Scanner(file);
             
             while (scanner.hasNextLine()) {
-                originalMessageBuilder.append(scanner.nextLine()).append("\n"); 
+                stringBuilder.append(scanner.nextLine()).append("\n"); 
             }
-            scanner.close(); // Close the scanner
+            scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
-        // Convert StringBuilder to String and trim the trailing newline
-        String originalMessage = originalMessageBuilder.toString().trim();
+        String fileContents = stringBuilder.toString().trim();
+        return fileContents;
+    }
 
-        String encryptedMessage = cipher.encrypt(originalMessage, keyFilename);
-        String decryptedMessage = cipher.decrypt(encryptedMessage, keyFilename);
+    public static void main(String[] args) {
+        VigenereCipher cipher = new VigenereCipher();
+        String messageFilename = "encrypt_check.txt";
+        String keyFilename = "key_check.txt";
+        String encryptedMessageCheckFilename = "decrypt_check.txt";
 
-        System.out.println("Original: " + originalMessage);
-        System.out.println("Encrypted: " + encryptedMessage);
-        System.out.println("Decrypted: " + decryptedMessage);
+        String originalMessage = cipher.getFileContents(messageFilename);
+        String encryptedMessage = cipher.encrypt(messageFilename, keyFilename);
+        String encryptedMessageCheck = cipher.getFileContents(encryptedMessageCheckFilename);
+        String decryptedMessage = cipher.decrypt(encryptedMessageCheckFilename, keyFilename);
+
+        System.out.println("ORIGINAL:\n" + originalMessage);
+        System.out.println("\nENCRYPTED:\n" + encryptedMessage);
+        System.out.println("\nENCRYPTED CHECK:\n" + encryptedMessageCheck);
+        System.out.println("\nDECRYPTED:\n" + decryptedMessage);
+
+        if (!encryptedMessage.equals(encryptedMessageCheck)) {
+            System.out.println("\nWARNING:\nEncrypted message does not match encrypted check!");
+        }
+        if (!decryptedMessage.equals(originalMessage)) {
+            System.out.println("\nWARNING:\nOriginal message does not match decrypted message!");
+        }
     }
 }
