22f2a77a6aa69a10395c38319f08a53122218a1c
ollie-vahid <olivervahid1@gmail.com>
2024-04-03 15:38:49 +0100

    Water UI done

--- PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
+++ PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c159e6d2c04bd3b091258f9573e18efe230629a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
