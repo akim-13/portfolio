ff859342f5161a1d7e37ba19cf6d6120faac50c2
JamesSheppardd <70041352+JamesSheppardd@users.noreply.github.com>
2024-04-04 23:49:25 +0100

    Added Steps UI

--- PI Systems/PI Systems/GUIs/StepsPage.xaml.cs
+++ PI Systems/PI Systems/GUIs/StepsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,29 @@ namespace PI_Systems.GUIs
         private void ToMenuButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.MainWindow.Content = MenuPage.instance;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void FitbitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Trace.WriteLine("Send to FitBit Auth");
+        }
+
+        private void GoalTimeframe_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            // James: The SelctionChanged calls this function whenever the selection changes (duh) but this also countswhenloading
+            // the page, so we gotta include the if statement to ignore this case.
+            if (!cb.IsLoaded)
+            {
+                return;
+            }
+            ComboBoxItem cbi = (cb).SelectedItem as ComboBoxItem;
+            Trace.WriteLine(cbi.Content.ToString());
         }
     }
 }
