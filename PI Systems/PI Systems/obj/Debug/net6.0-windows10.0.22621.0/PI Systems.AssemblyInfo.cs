2f9b16566d9bf8e75db35cff6b70e7f0eaf33549
Jeet <83248565+Lemony-Juicy@users.noreply.github.com>
2024-04-06 20:46:01 +0100

    More obj folder changes

--- PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
+++ PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8fcb6bd9bcd6c551935dc60cd00ad754b454f05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f86aa43af168bb524b3c4688eadef32e675eebb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
