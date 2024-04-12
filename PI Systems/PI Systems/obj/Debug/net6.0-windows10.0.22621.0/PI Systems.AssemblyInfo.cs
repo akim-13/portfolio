0b9d57052896f714dad7d58e1f0a0bf7c8d3f9ec
Jeet <83248565+Lemony-Juicy@users.noreply.github.com>
2024-04-12 12:29:29 +0100

    merging menu

--- PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
+++ PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec08421eb6171ac17393ac0e5717ee8aadc401fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e5fdd24c432a8ecefa61627aefda4ddd9102cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
