b9d92b4eadc29aeba195442214b8ce74947940ee
kkater-yna <k.dzemliuk@gmail.com>
2024-04-23 13:44:52 +0100

    Achievements functionality

--- PI Systems/PI Systems/GUIs/UserControls/Achievements.xaml.cs
+++ PI Systems/PI Systems/GUIs/UserControls/Achievements.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -18,11 +20,111 @@ namespace PI_Systems.GUIs.UserControls
     /// <summary>
     /// Interaction logic for Achievements.xaml
     /// </summary>
-    public partial class Achievements : UserControl
-    {
+    public partial class Achievements : UserControl { 
+
+        public int sleepAchievement;
+        public int stepsAchievement;
+        public int workAchievement;
+        public int waterAchievement;
+
+        public int achievementCounter = 0;
+
         public Achievements()
         {
             InitializeComponent();
+            UpdateSquareColor();
+            UpdateCurrentStrike();
+            UpdateWeekAchievements();
         }
+
+        private void UpdateSquareColor()
+        {
+            bool condition2 = true;
+            bool condition3 = true;
+            bool condition4 = true;
+
+            string getAchSleep;
+            string getAchStep;
+            string getAchWork;
+            string getAchWater;
+
+            string achievementMessage = "";
+
+            // Katrya: gets sleep data
+            getAchSleep = Database.Instance.GetStringDataToday<UserSleep>();
+            int achSleep = int.Parse(getAchSleep);
+
+            getAchStep = Database.Instance.GetStringDataToday<UserSteps>();
+            int achStep = int.Parse(getAchStep);
+
+            getAchWater = Database.Instance.GetStringDataToday<UserWater>();
+            int achWater = int.Parse(getAchWater);
+
+            getAchWork = Database.Instance.GetStringDataToday<UserGoals>();
+            int achWork = int.Parse(getAchWork);
+
+
+            if (achSleep>8)
+            {
+                Achievement1.Fill = Brushes.GreenYellow;
+                achievementCounter++;
+                achievementMessage += "You slept enought hours today!\n";
+            }
+            else
+            {
+                achievementMessage += "You could've slept more today!\n";
+            }
+            if (achWork>=2)
+            {
+                Achievement2.Fill = Brushes.GreenYellow;
+                achievementCounter++;
+                achievementMessage += "You worked for 2 hours today!\n";
+            }
+            else
+            {
+                achievementMessage += "You didn't manage to do much work today!\n";
+            }
+            if (achStep >= 60000)
+            {
+                Achievement3.Fill = Brushes.GreenYellow;
+                achievementCounter++;
+                achievementMessage += "You've completed steps goal for today!\n";
+            }
+            else
+            {
+                achievementMessage += "You can do more steps today!\n";
+            }
+            if (achWater>=2)
+            {
+                Achievement4.Fill = Brushes.GreenYellow;
+                achievementCounter++;
+                achievementMessage += "You drank enough water today!\n";
+            }
+            else
+            {
+                achievementMessage += "Keep drinking more water!\n";
+            }
+            if (achievementCounter ==4)
+            {
+                achievementMessage = "Congratulations! \nYou have completed \nall the goals for today!";
+            }
+            printNote(achievementMessage);
+
+        }
+
+        private void UpdateCurrentStrike()
+        {
+            
+        }
+
+        private void UpdateWeekAchievements()
+        {
+            WeeklyAchievementsLabel.Content = achievementCounter + "/4";
+        }
+        public void printNote(string message)
+        {
+            motivationalNote.Content = message;
+        }
+
     }
 }
--- PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
+++ PI Systems/PI Systems/obj/Debug/net6.0-windows10.0.22621.0/PI Systems.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944751cd0ae47a42d7fe2714a0ef4a3a9706b4f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d250b32ebd82c3db3e73af7a3a1dc1522c1788af")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI Systems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
