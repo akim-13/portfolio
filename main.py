f958c0153ec755f184cebe93924f40a7a689e924
Akim <akimkom@protonmail.com>
2024-04-27 15:30:36 +0100

    Implemented Layer, started ActivationFunction

--- main.py
+++ main.py
@@ -1,25 +1,93 @@
-# This skeleton code simply classifies every input as ham
-#
-# Here you can see there is a parameter k that is unused, the
-# point is to show you how you could set up your own. You might
-# also pass in extra data via a train method (also does nothing
-# here). Modify this code as much as you like so long as the 
-# accuracy test in the cell below runs.
+import numpy as np
+
+
+class Layer:
+    def __init__(self, num_of_inputs, num_of_neurons):
+        self.weights = 0.01 * np.random.randn(num_of_inputs, num_of_neurons)
+        self.biases = np.zeros((1, num_of_neurons))
+
+
+    def forward_pass(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+        return self.output
+
+
+class ActivationFunction:
+    def __init__(self, inputs, is_for_hidden_layer=True, current_layer=None):
+        self.inputs = inputs
+
+        if is_for_hidden_layer:
+            self.output = self.activate_rectifier()
+        else:
+            self.output = self.activate_sigmoid(current_layer)
+
+            
+    # A.K.A. ReLU
+    def activate_rectifier(self):
+        return np.maximum(0, self.inputs)
+
+
+    def activate_sigmoid(self, current_layer):
+        # You forgot to pass `current_layer` if this it is true.
+        if not current_layer:
+            # Do something.
+            return None
+
+        # NOPE, CONTINUE HERE
+        return 1 / (1 + np.exp**(-current_layer.output))
+        
 
 class SpamClassifier:
     def __init__(self, k):
         self.k = k
+        features, labels = None, None
+
+
+    def populate_features_and_labels(self, is_training_data):
+        if is_training_data:
+            dataset_filename = 'training_spam.csv'
+        else:
+            dataset_filename = 'test_spam.csv'
+
+        dataset_relative_path = f'./data/{dataset_filename}'
+
+        try:
+            dataset = np.loadtxt(open(dataset_relative_path), delimiter=',').astype(int)
+        except FileNotFoundError:
+            print(f'ERROR: "{dataset_relative_path}" file not found.')
+            return
+
+        self.labels = dataset[:, 0]
+        self.features = dataset[:, 1:]
+
+        print("Shape of the spam training data set:", dataset.shape)
+        print(dataset)
+        print("Shape of labels:", self.labels.shape)
+        print(self.labels)
+        print("Shape of features:", self.features.shape)
+        print(self.features)
+
         
     def train(self):
-        pass
+        self.populate_features_and_labels(True)
+        # first_layer = Layer
+        # Unsure whether it's supposed to be self.features here.
+        ActivationFunction(self.features, False)
         
-    def predict(self, data):
-        return np.zeros(data.shape[0])
-    
 
+    def predict(self, data):
+        self.populate_features_and_labels(False)
+        return np.zeros(data.shape[0])
+
+    
 def create_classifier():
     classifier = SpamClassifier(k=1)
     classifier.train()
     return classifier
 
-classifier = create_classifier()
+
+def main():
+    classifier = create_classifier()
+
+if __name__ == "__main__":
+    main()
